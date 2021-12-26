33c7a04e94c7538ad44cb24ebb0619700f15d539
Daniel Elias <danl3654321@gmail.com>
2021-12-26 20:44:14 +0200

    26.12.2021

--- aplication-project/daniel-app/src/app/in-memory-data.service.ts
+++ aplication-project/daniel-app/src/app/in-memory-data.service.ts
@@ -8,14 +8,13 @@ import { Employee } from './employte';
 
 export class InMemoryDataService implements InMemoryDbService {
 createDb() {
-
 const employees = [
-    { id: 12768733, fname: 'John', lname: 'Tucker', dateofbirth: '31/12/77', salary: 5400, email: 'john@gmail.com' },
-    { id: 12768734, fname: 'Adi', lname: 'Ashkenazi', dateofbirth: '30/11/79', salary: 6400, email: 'adi@gmail.com' },
+    { id: 12768733, fname: 'John', lname: 'Tucker', dateofbirth: '12/31/77', salary: 5400, email: 'john@gmail.com'  },
+    { id: 12768734, fname: 'Adi', lname: 'Ashkenazi', dateofbirth: '11/30/79', salary: 6400, email: 'adi@gmail.com' },
     { id: 12768735, fname: 'Zion', lname: 'Turgeman', dateofbirth: '4/11/73', salary: 5600, email: 'zion@gmail.com' },
     { id: 12768736, fname: 'Paul', lname: 'Rudd', dateofbirth: '6/1/80', salary: 5600, email: 'paul@gmail.com' },
-    { id: 12768737, fname: 'Maggi', lname: 'Berko', dateofbirth: '30/6/56', salary: 10400, email: 'maggib@gmail.com' },
-    { id: 12768738, fname: 'Ruth', lname: 'Glik', dateofbirth: '30/7/59', salary: 7300, email: 'glick23@gmail.com' },
+    { id: 12768737, fname: 'Maggi', lname: 'Berko', dateofbirth: '6/20/56', salary: 10400, email: 'maggib@gmail.com' },
+    { id: 12768738, fname: 'Ruth', lname: 'Glik', dateofbirth: '3/7/59', salary: 7300, email: 'glick23@gmail.com' },
     { id: 12768739, fname: 'David', lname: 'Duhov', dateofbirth: '1/1/88', salary: 7200, email: 'david@gmail.com'},
     { id: 12768740, fname: 'Meir', lname: 'Alul', dateofbirth: '1/12/87', salary: 3400, email: 'meir@gmail.com' },
     { id: 12768741, fname: 'Mor', lname: 'Sharon', dateofbirth: '11/10/88', salary: 5600, email: 'morsharon@gmail.com' },
